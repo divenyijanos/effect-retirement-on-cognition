483063dcabd06e1a196d0c2419709684911e8a05
Janos Divenyi <janos.divenyi@emarsys.com>
2020-08-23 09:04:46 +0200

    ♻️ separate theme setter

--- code/panel_graph_in_R.R
+++ code/panel_graph_in_R.R
@@ -3,21 +3,7 @@ library(data.table)
 library(magrittr)
 library(ggplot2)
 
-MAIN_COLOR <- "#042037"
-SECONDARY_COLOR <- "#D4A86A"
-THIRD_COLOR <- "#3F88C5"
-
-mytheme <- function (base_size = 11, base_family = "", base_line_size = base_size/22,
-    base_rect_size = base_size/22) {
-    theme_minimal(base_size = base_size, base_family = base_family,
-        base_line_size = base_line_size, base_rect_size = base_rect_size) %+replace%
-        theme(
-            panel.border = element_rect(fill = NA, colour = "grey20"),
-            panel.grid = element_line(colour = "grey92"),
-            panel.grid.minor = element_line(size = rel(0.5))
-        )
-}
-theme_set(mytheme())
+source("code/set_my_theme.R")
 
 data <- read.dta13("data/derived.dta") %>% as.data.table()
 
